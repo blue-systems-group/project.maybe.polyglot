ffacdf78b714891cb836880d65a1e5e0a1f2b1ff
xcv58 <chenyihonglove@gmail.com>
2015-08-12 11:50:16 -0400

    add maybe syntax in Hello.java

--- maybe/tests/Hello.java
+++ maybe/tests/Hello.java
@@ -1,5 +1,21 @@
 public class Hello {
     public static void main(String[] args) {
+        int a = maybe ("1") 1, 2, 3;
+
+        maybe("2") {
+            int b = 0;
+        } or {
+            int b = 1;
+        }
+
+        maybe ("3") {
+            int a = 0;
+        } or {
+            int b = 1;
+        } or {
+            int c = 2;
+        }
+
         System.out.println("Hello world!");
     }
 }
